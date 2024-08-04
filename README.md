5200b7ff72f67cd3d36cf8f5da7947201b9cfc2c
meleu <meleu.dev@gmail.com>
2024-08-04 00:57:52 -0300

    add script

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # medals.sh
-Check the Olympics 2024 medal table from the comfort of your terminal
+
+Check the Olympics 2024 medal table from the comfort of your terminal.
+
+Data comes from <https://olympics.com/en/paris-2024/medals>.
+
+## Dependencies
+
+- [curl](https://curl.se/): to get the data
+- [htmlq](https://github.com/mgdm/htmlq): to parse contents from an HTML file
+- [jq](https://jqlang.github.io/jq): to parse JSON data
+
+Assuming you have [Homebrew](https://brew.sh) installed,
+you can install the dependencies with:
+
+```bash
+brew install curl
+brew install htmlq
+brew install jq
+```
+
+## Installation
+
+Just copy [medals.sh](./medals.sh) and put it anywhere in your PATH.
+
+Here's a suggestion:
+
+```bash
+mkdir -p ~/.local/bin
+curl 'https://raw.githubusercontent.com/meleu/medals.sh/main/medals.sh' > ~/.local/bin/medals
+chmod a+x ~/.local/bin/medals
+```
+
+## Usage
+
+```bash
+# get the top 10 (default amount)
+medals
+
+# get the top 15
+medals 15
+```
+
+Examples:
+
+![Made with VHS](https://vhs.charm.sh/vhs-4m8XRluYAkwcrJfu4Q2lQf.gif)
---
+++ medals.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/env bash
+# medals.sh
+###########
+# Show the Olympics 2024 medal table.
+#
+# Data from <https://olympics.com/en/paris-2024/medals>
+#
+# Dependencies:
+# - curl
+# - jq
+# - htmlq <https://github.com/mgdm/htmlq> (`brew install htmlq`)
+#
+# meleu - <https://meleu.dev/>
+
+set -Eeuo pipefail
+trap 'echo "[ERROR]: ${BASH_SOURCE}:${FUNCNAME}:${LINENO}"' ERR
+
+readonly URL='https://olympics.com/en/paris-2024/medals'
+
+readonly DEPENDENCIES=(curl jq htmlq)
+
+main() {
+  # shellcheck disable=2155
+  local inputFile="$(mktemp)"
+  local jsonFile="${inputFile}.json"
+  local amount="${1:-10}"
+
+  checkDependencies
+
+  echo -en "Getting data...\r" >&2
+  getMedalPage "$inputFile"
+  getJsonData "$inputFile" "$amount" > "$jsonFile"
+  renderMedalTable "$jsonFile"
+
+  rm -rf "$inputFile" "$jsonFile"
+}
+
+getMedalPage() {
+  local file="$1"
+
+  # olympics.com does not accept curl as an user-agent,
+  # then we need to send a custom one.
+  curl "$URL" \
+    --silent \
+    --location \
+    --user-agent "Mozilla/5.0" \
+    --output "$file"
+}
+
+getJsonData() {
+  local file="$1"
+  local max="${2:-10}"
+  local jqFilter
+
+  jqFilter="
+    .props.pageProps.initialMedals.medalStandings.medalsTable[:${max}]
+      | [ .[]
+          | {
+            description,
+            medalsNumber: [.medalsNumber[] | select(.type == \"Total\")]
+          }
+        ]
+      | [.[] | {description, medalsNumber: .medalsNumber[0]}]
+  "
+
+  htmlq --text 'script#__NEXT_DATA__' --filename "$file" \
+    | jq "${jqFilter}"
+}
+
+renderMedalTable() {
+  local jsonFile="$1"
+  local medalTableData
+  local jqFilter
+  local line
+  local rank=0
+  local gold silver bronze total country
+
+  jqFilter='.[] |
+    "\(
+      .medalsNumber.gold
+    ):\(
+      .medalsNumber.silver
+    ):\(
+      .medalsNumber.bronze
+    ):\(
+      .medalsNumber.total
+    ):\(
+      .description
+    )"
+  '
+
+  medalTableData="$(jq --raw-output "$jqFilter" "$jsonFile")"
+
+  echo " rank |  🥇 |  🥈 |  🥉 | total | Country"
+  echo "------|-----|-----|-----|-------|---------"
+  while IFS= read -r line || [[ -n "${line}" ]]; do
+    gold="$(cut -d: -f1 <<< "$line")"
+    silver="$(cut -d: -f2 <<< "$line")"
+    bronze="$(cut -d: -f3 <<< "$line")"
+    total="$(cut -d: -f4 <<< "$line")"
+    country="$(cut -d: -f5 <<< "$line")"
+
+    printf " %4s | %3s | %3s | %3s | %5s | %s\n" \
+      "$((++rank))" "$gold" "$silver" "$bronze" "$total" "$country"
+  done <<< "$medalTableData"
+
+  echo -e "\nSource: $URL"
+}
+
+checkDependencies() {
+  local bin
+  local failure=false
+
+  for bin in "${DEPENDENCIES[@]}"; do
+    command -v "$bin" > /dev/null && continue
+    echo "[ERROR]: '$bin' is not in your PATH"
+    failure=true
+  done
+
+  [[ "$failure" == 'false' ]]
+}
+
+main "$@"
